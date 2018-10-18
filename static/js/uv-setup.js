885f4941d1630392adfc13fecd5c3c0ad6939877
edsilv <e.silverton@gmail.com>
2018-10-18 16:53:17 +0100

    de-dupe uv params

--- static/js/uv-setup.js
+++ static/js/uv-setup.js
@@ -1,26 +1,25 @@
-var uv, $uv, urlDataProvider;
+var uv, $uv, urlDataProvider, data, manifest;
 
-var root = '../static/uv';
-var configUri = '../static/uv.json';
-var locales = [{ name: 'en-GB' }];
+data = {
+    root: '../static/uv',
+    configUri: '../static/uv.json',
+    locales: [{ name: 'en-GB' }]
+}
 
-function openManifest(manifest) {
+function openManifest() {
 
     window.scrollTo(0, 0);
 
-    $uv.slideDown("fast", function () {
-        uv.set({
-                root: root,
-                iiifResourceUri: manifest,
-                configUri: configUri,
+    // set manifest and reset hash params
+    $uv.slideDown('fast', function () {
+        uv.set(Object.assign({}, data, {
                 iiifResourceUri: manifest,
                 collectionIndex: 0,
                 manifestIndex: 0,
                 sequenceIndex: 0,
-                canvasIndex: 0,
-                locales: locales
+                canvasIndex: 0
             }
-        );
+        ));
     });
 }
 
@@ -30,18 +29,15 @@ window.addEventListener('uvLoaded', function (e) {
 
     urlDataProvider = new UV.URLDataProvider();
 
-    var data = {
-        root: root,
-        iiifResourceUri: manifest,
-        configUri: configUri,
+    // set initial hash params
+    data = Object.assign({}, data, {
         collectionIndex: Number(urlDataProvider.get('c', 0)),
         manifestIndex: Number(urlDataProvider.get('m', 0)),
         sequenceIndex: Number(urlDataProvider.get('s', 0)),
         canvasIndex: Number(urlDataProvider.get('cv', 0)),
         rotation: Number(urlDataProvider.get('r', 0)),
-        xywh: urlDataProvider.get('xywh', ''),
-        locales: locales
-    };
+        xywh: urlDataProvider.get('xywh', '')
+    });
 
     uv = createUV('#uv', data, urlDataProvider);
 
@@ -52,18 +48,20 @@ window.addEventListener('uvLoaded', function (e) {
     const iiifGallery = document.querySelector('iiif-gallery');
 
     iiifGallery.addEventListener('onSelectManifest', function (evt) {
-        openManifest(evt.detail.id);
+        manifest = evt.detail.id;
+        openManifest();
     });
 
     iiifGallery.addEventListener('onSelectCollection', function (evt) {
-        openManifest(evt.detail.id);
+        manifest = evt.detail.id;
+        openManifest();
     });
 
     // are there uv hash parameters?
-    var manifest = Utils.Urls.getHashParameter('manifest');
+    manifest = Utils.Urls.getHashParameter('manifest');
 
     if (manifest) {
-        openManifest(manifest);
+        openManifest();
     }
 
 }, false);
