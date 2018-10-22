07ddf2a3438118ea5251e9887bb4b2aeb9fd25f4
edsilv <e.silverton@gmail.com>
2018-10-22 15:41:48 +0100

    use window.uvstate

--- components/uvcomponent.js
+++ components/uvcomponent.js
@@ -11,20 +11,17 @@ export default class UVComponent extends Component {
 
         window.scrollTo(0, 0);
     
-        // set manifest and reset hash params
-        // $uv.slideDown('fast', function () {
-
         document.querySelector('.uv').style.display = 'block';
 
         window.uv.set({
-            root: window.root,
-            configUri: window.configUri,
-            iiifResourceUri: window.manifest,
+            root: window.uvstate.root,
+            configUri: window.uvstate.configUri,
+            iiifResourceUri: window.uvstate.iiifResourceUri,
             collectionIndex: 0,
             manifestIndex: 0,
             sequenceIndex: 0,
             canvasIndex: 0,
-            locales: [{ name: 'en-GB' }]
+            locales: window.uvstate.locales
         });
     }
 
@@ -34,32 +31,36 @@ export default class UVComponent extends Component {
 
         window.addEventListener('uvLoaded', function (e) {
 
-            window.urlDataProvider = new UV.URLDataProvider();
-            window.root = that.props.root;
-            window.configUri = that.props.configUri;
+            window.uvstate = {
+                urlDataProvider: new UV.URLDataProvider(),
+                root: that.props.root,
+                configUri: that.props.configUri,
+                locales: [{ name: 'en-GB' }],
+                iiifResourceUri: ''
+            }
 
             var data = {
-                root: window.root,
-                configUri: window.configUri,
-                collectionIndex: Number(window.urlDataProvider.get('c', 0)),
-                manifestIndex: Number(window.urlDataProvider.get('m', 0)),
-                sequenceIndex: Number(window.urlDataProvider.get('s', 0)),
-                canvasIndex: Number(window.urlDataProvider.get('cv', 0)),
-                rotation: Number(window.urlDataProvider.get('r', 0)),
-                xywh: window.urlDataProvider.get('xywh', ''),
-                locales: [{ name: 'en-GB' }]
+                root: window.uvstate.root,
+                configUri: window.uvstate.configUri,
+                collectionIndex: Number(window.uvstate.urlDataProvider.get('c', 0)),
+                manifestIndex: Number(window.uvstate.urlDataProvider.get('m', 0)),
+                sequenceIndex: Number(window.uvstate.urlDataProvider.get('s', 0)),
+                canvasIndex: Number(window.uvstate.urlDataProvider.get('cv', 0)),
+                rotation: Number(window.uvstate.urlDataProvider.get('r', 0)),
+                xywh: window.uvstate.urlDataProvider.get('xywh', ''),
+                locales: window.uvstate.locales
             };
         
-            window.uv = createUV('#uv', data, window.urlDataProvider);
+            window.uv = createUV('#uv', data, window.uvstate.urlDataProvider);
         
             window.uv.on('created', function () {
-                Utils.Urls.setHashParameter('manifest', window.manifest);
+                Utils.Urls.setHashParameter('manifest', window.uvstate.iiifResourceUri);
             });
 
             // are there uv hash parameters?
-            window.manifest = Utils.Urls.getHashParameter('manifest');
+            window.uvstate.iiifResourceUri = Utils.Urls.getHashParameter('manifest');
         
-            if (window.manifest) {
+            if (window.uvstate.iiifResourceUri) {
                 that.openManifest();
             }
         
@@ -68,7 +69,7 @@ export default class UVComponent extends Component {
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.manifest) {
-            window.manifest = nextProps.manifest;
+            window.uvstate.iiifResourceUri = nextProps.manifest;
             this.openManifest();
         }
     }
