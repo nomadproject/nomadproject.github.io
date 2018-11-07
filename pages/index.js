ab54f91f8b18739d9133bfd9e2d8b3125fa734f3
edsilv <e.silverton@gmail.com>
2018-11-07 12:33:24 +0000

    build

--- pages/index.js
+++ pages/index.js
@@ -119,7 +119,7 @@ export default class Home extends Component {
                             <p class="lh-copy mw8 center firasans" dangerouslySetInnerHTML={{ __html: this.state.intro.text }}></p>
                         </div>
                         <div class="cf mw7 center">
-                            <a class="link dim gray dib h2 w2 br-100 mr3 " href="https://twitter.com/hashtag/nomadproject?src=hash"
+                            <a class="link dim gray dib h2 w2 br-100 mr3 " href="https://twitter.com/i/moments/1059806320642138112"
                                 title="">
                                 <svg data-icon="twitter" viewBox="0 0 32 32" fill="currentColor">
                                     <path d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4"></path>
