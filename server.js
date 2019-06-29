fc63c5ffdeba3d8fbfbecda6b3d226d49f45b4dd
Edward Silverton <e.silverton@gmail.com>
2019-06-30 00:09:54 +0100

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
- 
+
 app.prepare().then(() => {
     createServer((req, res) => {
         handle(req, res)
