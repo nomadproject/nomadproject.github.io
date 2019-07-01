d1fe657ee6c0090f4432ca80578ec922de81bb4c
Edward Silverton <e.silverton@gmail.com>
2019-07-01 08:29:58 +0100

    false

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const { parse } = require('url')
 const next = require('next')
 const port = process.env.PORT || 3000
 //const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev: false })
+const dev = false
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
